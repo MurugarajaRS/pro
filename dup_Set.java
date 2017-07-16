d2a084b8b466cc7dabbd070201a654fb544ca890
MurugarajaRS <hutsraja@gmail.com>
2017-07-16 08:08:21 +0530

    Create dup_Set.java

---
+++ dup_Set.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class dup_Set
+{
+    public static void main(String sr[])
+    {
+        Scanner scan = new Scanner(System.in);
+        String str[] = scan.next().split("");
+        HashSet<String> set = new HashSet<String>();
+        for(int i=0;i<str.length;i++)
+            set.add(str[i]);
+        System.out.print(set);
+    }
+}
