02c98946f02dcb2e97af42547ac753399fc9b5bd
MurugarajaRS <hutsraja@gmail.com>
2018-11-16 14:45:59 +0530

    Create word_count.java

---
+++ word_count.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class word_count{
+
+     public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
+        String str[] = scan.nextLine().split(" ");
+        System.out.println(str.length;
+     }
+}
