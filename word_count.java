2fa932cae3a929a92e6bd12d8fc0976ff8b905d7
MurugarajaRS <hutsraja@gmail.com>
2018-11-16 14:46:44 +0530

    Update word_count.java

--- word_count.java
+++ word_count.java
@@ -4,6 +4,6 @@ public class word_count{
      public static void main(String []args){
         Scanner scan = new Scanner(System.in);
         String str[] = scan.nextLine().split(" ");
-        System.out.println(str.length;
+        System.out.println(str.length);
      }
 }
