f7b57b1b8c98ccd81d6e00abfe134107c1524d45
MurugarajaRS <hutsraja@gmail.com>
2018-11-16 14:43:42 +0530

    Create Space_removal_count.java

---
+++ Space_removal_count.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Space_removal_count{
+
+     public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        str=str.replaceAll(" ","");
+        System.out.println(str.length());
+        
+     }
+}
